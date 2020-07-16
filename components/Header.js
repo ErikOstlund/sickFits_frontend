4415882953268944e44b0827910a2fe009e099dd
Erik Ostlund <eostlund74@gmail.com>
2020-07-16 10:08:29 -0500

    Added progress bar
    Using NProgress for simple progress bar on page load.

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,23 @@
-import Nav from './Nav';
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import styled from 'styled-components';
+import Nav from './Nav';
 
+// Route Events (new page load)
+Router.onRouteChangeStart = () => {
+	NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+	NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+	NProgress.done();
+};
+
+// Styles
 const Logo = styled.h1`
 	font-size: 4rem;
 	margin-right: 2rem;
