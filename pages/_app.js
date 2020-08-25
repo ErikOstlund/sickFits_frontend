0263763368a84bd30c32d50099cd4a82b5d2a5fb
Erik Ostlund <eostlund74@gmail.com>
2020-08-24 19:58:12 -0500

    Setup Apollo Client with React App

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,35 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+	// this chunk is for page numbers within the app
+	// performs all queries for given page and returns data in pageProps!
+	static async getInitialProps({ Component, ctx }) {
+		let pageProps = {};
+		if (Component.getInitialProps) {
+			pageProps = await Component.getInitialProps(ctx);
+		}
+		// expose the query to the user
+		pageProps.query = ctx.query;
+		return { pageProps };
+	}
+
 	render() {
-		const { Component } = this.props;
+		const { Component, apollo, pageProps } = this.props;
 
 		return (
 			<Container>
-				<Page>
-					{/* The Component tag will be the current page (home, sell, orders) */}
-					<Component />
-				</Page>
+				<ApolloProvider client={apollo}>
+					<Page>
+						{/* The Component tag will be the current page (home, sell, orders) */}
+						<Component {...pageProps} />
+					</Page>
+				</ApolloProvider>
 			</Container>
 		);
 	}
 }
 
-export default MyApp;
+export default withData(MyApp);
